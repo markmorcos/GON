d556eebf46e3c034d96c8fa498578db4e7795fcc
ebram <ebram.sherif@gmail.com>
2015-12-14 20:51:25 +0200

    devise sign up trial

---
+++ app/controllers/api/registrations_controller.rb
@@ -0,0 +1,20 @@
+class Api::RegistrationsController < Api::BaseController
+  
+  respond_to :json
+  def create
+
+    @user = User.new(params[:user,:first_name,:last_name,:password])
+    if user.save
+      render :json=> @user.as_json(:auth_token=>@user.authentication_token, :email=>@user.email
+        ,:password=>@user.password,:last_name=>@user.last_name,:first_name=>@user.first_name), :status=>201
+      return
+    else
+      warden.custom_failure!
+      render :json=> @user.errors, :status=>422
+    end
+  end
+  def registrations_params
+    params.require(:registration).permit(
+      :first_name,:last_name,:email,:password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  resources :likes
+  
+  namespace :api, defaults: {format :json} do
+    devise_for :users
+    resources :likes
+    resources :comments
+    resources :friends
+    resources :messages
+    resources :posts
+  end
 
-  resources :comments
 
-  resources :friends
-
-  resources :messages
-
-  resources :posts
-
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
