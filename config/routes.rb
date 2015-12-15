6678cfc5fa03119ed709c64f6d1bfb209e242cbb
ebram <ebram.sherif@gmail.com>
2015-12-15 11:00:59 +0200

    fixed routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  namespace :api, defaults: {format :json} do
+  namespace :api, defaults: {format: :json} do
     devise_for :users
     resources :likes
     resources :comments
