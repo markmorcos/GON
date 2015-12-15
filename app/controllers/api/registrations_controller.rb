5ef41c3fdc900d2231893ea6201b40d140031a0f
ebram <ebram.sherif@gmail.com>
2015-12-15 10:54:01 +0200

    added sessions controller

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -7,7 +7,7 @@ class Api::RegistrationsController < Api::BaseController
     if user.save
       render :json=> @user.as_json(:auth_token=>@user.authentication_token, :email=>@user.email
         ,:password=>@user.password,:last_name=>@user.last_name,:first_name=>@user.first_name), :status=>201
-      respond_with @user
+      return
     else
       warden.custom_failure!
       render :json=> @user.errors, :status=>422
